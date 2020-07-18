a8d8fc58d70a885c60e239bb3daaf34412d186ee
rahul <ra19hul1995@gmail.com>
2020-07-18 15:31:33 +0530

    hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+//const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 const app=express();
 
 app.use(express.static("public"));
@@ -18,7 +19,7 @@ const userSchema=new mongoose.Schema({
   pass:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['pass'] });
+//userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['pass'] });
 
 const User=new mongoose.model("User",userSchema);
 
@@ -36,7 +37,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    pass:req.body.password
+    pass:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -49,7 +50,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err){
